635c1e961dd3ad98aecc59eb565253522199c5d5
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-11 17:15:50 -0500

    buttons displaying, numbers display as well

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -6,6 +6,9 @@ import "./App.css";
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 import Display from "./components/DisplayComponents/Display";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -14,14 +17,22 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function  to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  const [displayValue, setDisplayValue] = useState("0");
+  const addNumber = (number) => {
+    setDisplayValue(displayValue => displayValue + number);
+  }
+
   return (
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export //all files */}
-        
+
         {/* This is a function. It has an object with a key value pair It does this: {Display({number: 0})}.....number is a prop, with a key of 0....all props end up in this object*/}
-        <Display number={0}/>
+        <Display number={displayValue}/>
+        <Numbers addNumber={addNumber}/>
+        <Operators />
+        <Specials />
       </div>
     </div>
   );
