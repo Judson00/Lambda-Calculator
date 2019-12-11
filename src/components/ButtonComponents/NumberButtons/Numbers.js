ecb02322d0448f56e415011b0200e4d3661aabaa
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-10 19:09:17 -0500

    refactor of number components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,25 +1,22 @@
-import React, {useState} from "react";
-import NumberButton from "./NumberButton";
+import React from "react";
+import {numbers} from "../../../data";
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 
-//import { numbers } from '../../../data'; 
+// import { numbers } from '../../../data'; 
 
 //Import your array data to from the provided data file
 
-const Numbers = (props) => {
+const Numbers = (numberButtons) => {
   // STEP 2 - add the imported data to state
-  const {numbers} = props
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-      {numbers.map(num => {
-        return <NumberButton number={num} key={num.toString()}/>
-      })}
+      {numberButtons = numbers.map((number) => <button>{number}</button>)}
     </div>
   );
 };
 
-export default Numbers
+export default Numbers;
